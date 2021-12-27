ffdcae442d3a3bcac87d3936c96b792e8c8647d2
Nathan Lewis <nlewis@grovergaming.com>
2021-12-27 16:31:12 -0500

    Add Command to Update Title

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace Roster.Client.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
+        private string _title = "Roster App";
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public Command UpdateApplicationCommand { get; }
 
-        public string Title { get; set; } = "Roster App";
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public HomeViewModel()
+        {
+            UpdateApplicationCommand = new Command(() => Title = "Roster App (v2.0)");
+        }
     }
 }

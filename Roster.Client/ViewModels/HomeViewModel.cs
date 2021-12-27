ca054d98b36536a920606312254f22fbdaa4ecac
Nathan Lewis <nlewis@grovergaming.com>
2021-12-27 16:46:26 -0500

    Add Person Model and Add People Observable Collection to HomeViewModel

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -1,25 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.ComponentModel;
 using Xamarin.Forms;
-using System.Diagnostics;
+using Roster.Client.Models;
 
 namespace Roster.Client.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
-        private string _title = "Roster App";
+        public HomeViewModel()
+        {
+            UpdateApplicationCommand = new Command(() => Title = "Roster App (v2.0)");
+
+            People = new ObservableCollection<Person>
+            {
+                new Person(){ Name = "Delores Feil", Company = "Legros Group" },
+                new Person(){ Name = "Ann Zboncak", Company = "Ledner - Ferry" },
+                new Person(){ Name = "Jaime Lesch", Company = "Herzog and Sons"}
+            };
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void NotifyPropertyChanged(String propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
         public Command UpdateApplicationCommand { get; }
 
+        private string _title = "Roster App";
         public string Title
         {
             get
@@ -33,9 +39,11 @@ namespace Roster.Client.ViewModels
             }
         }
 
-        public HomeViewModel()
+        public ObservableCollection<Person> People { get; }
+
+        private void NotifyPropertyChanged(String propertyName = "")
         {
-            UpdateApplicationCommand = new Command(() => Title = "Roster App (v2.0)");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
